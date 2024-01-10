75f6f71bb9ee98cebdc31cc188073e86e526543e
ichetiva <feoktistovvladislav@outlook.com>
2024-01-10 13:43:11 +0300

    fixed

--- handlers.py
+++ handlers.py
@@ -1,14 +1,14 @@
-from aiogram import types, Router
+from aiogram import types
 from aiogram.types import WebAppInfo
-from aiogram.filters import CommandStart
+from aiogram.dispatcher.filters import CommandStart
 
-router = Router()
+from loader import dp
 
 
-@router.message(CommandStart)
-async def Start(m: types.Message):
+@dp.message_handler(CommandStart())
+async def start_handler(m: types.Message):
     markup = types.InlineKeyboardMarkup()
     markup.add(
         types.KeyboardButton('Играть', web_app=WebAppInfo(url="https://rise6up.com/"))
     )
-    await m.answer('Привет! Сыграть ты можешь нажав на кнопку', reply_markup=markup)
+    await m.answer('Привет! Сыграть ты можешь нажав на кнопку!', reply_markup=markup)
---
+++ main.py
@@ -0,0 +1,11 @@
+import logging
+
+from aiogram import executor
+
+from handlers import dp
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+
+    executor.start_polling(dp)
